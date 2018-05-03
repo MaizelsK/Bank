ba690866eb71f63495a8350b09a155eb123adc50
Майзельс Кирилл Игоревич <@corp.itstep>
2018-05-03 11:51:01 +0600

    Добавьте файлы проекта.

---
+++ Bank/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FisrtName { get; set; }
+        public string MiddleName { get; set; }
+        public string SurName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
+
